67382776540da10645a9e1df5a2eb81679d2a2d0
Aniket Bhattacharjee <devmike333@gmail.com>
2021-12-27 14:06:30 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,87 @@
+<?php
+$profpic = "images.png";
+?>
+
+<html> 
+
+<head> 
+
+  <script src='https://cdn.scaledrone.com/scaledrone.min.js'></script> 
+
+  <meta charset="utf-8"> 
+
+  <meta name="viewport" content="width=device-width"> 
+
+  <style> 
+
+    body { 
+
+      background-image: url('<?php echo $profpic;?>');
+
+      display: flex; 
+
+      height: 100vh; 
+
+      margin: 0; 
+
+      align-items: center; 
+
+      justify-content: center; 
+
+      padding: 0 50px; 
+
+      font-family: -apple-system, BlinkMacSystemFont, sans-serif; 
+
+    } 
+
+    video { 
+
+      max-width: calc(50% - 100px); 
+
+      margin: 0 50px; 
+
+      box-sizing: border-box; 
+
+      border-radius: 2px; 
+
+      padding: 0; 
+
+      background: white; 
+
+    } 
+
+    .copy { 
+
+      position: fixed; 
+
+      top: 10px; 
+
+      left: 50%; 
+
+      transform: translateX(-50%); 
+
+      font-size: 16px; 
+
+      color: white; 
+
+    } 
+
+  </style> 
+
+</head> 
+
+<body> 
+
+  <div class="copy">Get started with your meeting by sending your friend the link!</div> 
+
+  <video id="localVideo" autoplay muted></video> 
+
+  <video id="remoteVideo" autoplay></video> 
+
+   
+
+  <script src="script.js"></script> 
+
+</body> 
+
+</html> 
